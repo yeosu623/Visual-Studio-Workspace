a3c8557f7e8ed7d5981df2036871f2843983e578
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2023-10-18 05:47:52 +0900

    code update

--- DataStructurePractice6/main.c
+++ DataStructurePractice6/main.c
@@ -10,28 +10,57 @@ typedef struct _node {
 	struct _node* link;
 }node;
 
+int top = -1;
+int front_q = -1, rear_q = -1;
+
 void insert_front(node**, int);
 void print_list(node**);
 void insert_last(node**, int);
 int search(node**, int);
 int delete_front(node**);
 int delete_element(node**, int);
+void push(node**, int);
+int pop(node**);
+void add_q(node**, int);
+int delete_q(node**);
+void invert(node**);
+void merge(node**, node**);
+void sort(node**);
+void swap(int*, int*);
 
 int main() {
 	int i;
-	int result;
+	int input = 0;
 	node* A = NULL;
+	node* B = NULL;
 
-	for (i = 0; i < 10; i++) {
-		insert_last(&A, i*2);
+	while (true) {
+		printf("input data in A : ");
+		scanf_s("%d", &input);
+
+		if (input == -1) break;
+		else insert_last(&A, input);
 	}
-	print_list(&A);
-	printf("\n");
+	while (true) {
+		printf("input data in B : ");
+		scanf_s("%d", &input);
 
-	int n = delete_element(&A, 18);
-	printf("deleted = %d\n", n);
+		if (input == -1) break;
+		else insert_last(&B, input);
+	}
+
+	printf("A = ");
 	print_list(&A);
-	printf("\n");
+	printf("B = ");
+	print_list(&B);
+
+	merge(&A, &B);
+	printf("A+B = ");
+	print_list(&A);
+
+	sort(&A);
+	printf("sorted A+B = ");
+	print_list(&A);
 }
 
 void insert_front(node** A, int data) {
@@ -47,6 +76,8 @@ void print_list(node** A) {
 
 	for (ptr = *A; ptr != NULL; ptr = ptr->link)
 		printf("%d ", ptr->data);
+
+	printf("\n");
 }
 
 void insert_last(node** A, int data) {
@@ -144,4 +175,89 @@ int delete_element(node** A, int data) {
 			return data;
 		}
 	}
+}
+
+void push(node** A, int data) {
+	top++;
+	insert_front(A, data);
+}
+
+int pop(node** A) {
+	delete_front(A);
+	top--;
+}
+
+void add_q(node** A, int data) {
+	rear_q++;
+	insert_last(A, data);
+}
+
+int delete_q(node** A) {
+	front_q++;
+	delete_front(A);
+}
+
+void invert(node** A) {
+	node* ptr = *A;
+	node* ptr_next = NULL;
+	node* linkdata = NULL;
+
+	while (ptr != NULL) {
+		ptr_next = ptr->link;
+		ptr->link = linkdata;
+		linkdata = ptr;
+
+		ptr = ptr_next;
+	}
+
+	*A = linkdata;
+}
+
+void merge(node** merger, node** mergee) {
+	node* ptr;
+
+	for (ptr = *merger; ptr->link != NULL; ptr = ptr->link);
+	ptr->link = *mergee;
+}
+
+void sort(node** A) {
+	int i = 0, j, min;
+	int len = 0;
+	int* data;
+	node* ptr;
+
+	// get length of linked list
+	for (ptr = *A; ptr != NULL; ptr = ptr->link)
+		len++;
+
+	// get all data from linked list
+	data = (int*)malloc(sizeof(int) * len);
+
+	for (ptr = *A; ptr != NULL; ptr = ptr->link)
+		data[i++] = ptr->data;
+
+	// sort data
+	for (i = 0; i < len - 1; i++) {
+		min = i;
+		for (j = i + 1; j < len; j++)
+			if (data[j] < data[min])
+				min = j;
+
+		swap(&data[i], &data[min]);
+	}
+
+	// input sorted data in the linked list
+	i = 0;
+
+	for (ptr = *A; ptr != NULL; ptr = ptr->link)
+		ptr->data = data[i++];
+
+	// free dynamic allocated array
+	free(data);
+}
+
+void swap(int* x, int* y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
 }
