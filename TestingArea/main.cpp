df241e1846aa668557f6f4d33793b81d772dc774
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-02-06 00:20:55 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,65 +1,6 @@
 #include <iostream>
-#include <cstring>
+#include <cmath>
 using namespace std;
-
-int farm[50][50];
-int answer = 0;
-int first_dfs = 1;
-int dx[4] = { 0, -1, 1, 0 };
-int dy[4] = { 1, 0, 0, -1 };
-void dfs(int, int);
-void moving(int, int);
-void printArea();
-
-void dfs(int x, int y)
-{
-	if (!farm[x][y]) return;
-	else
-	{
-		farm[x][y] = 0;
-
-		if (first_dfs)
-		{
-			answer++;
-
-			first_dfs = 0;
-			moving(x, y);
-			first_dfs = 1;
-
-			//printArea();
-		}
-		else
-		{
-			moving(x, y);
-		}
-	}
-}
-
-void moving(int x, int y)
-{
-	for (int i = 0; i < 4; i++)
-	{
-		if (x + dx[i] == -1 || x + dx[i] == 50) continue;
-		if (y + dy[i] == -1 || y + dy[i] == 50) continue;
-
-		dfs(x + dx[i], y + dy[i]);
-	}
-}
-
-void printArea()
-{
-	cout << "answer : " << answer << '\n';
-	for (int i = 0; i < 11; i++)
-	{
-		for (int j = 0; j < 11; j++)
-		{
-			if (farm[i][j]) cout << 'O';
-			else cout << '.';
-		}
-		cout << '\n';
-	}
-	cout << '\n' << '\n';
-}
 
 int main()
 {
@@ -67,29 +8,38 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int t;
-	cin >> t;
+	int n, r, c;
+	cin >> n >> r >> c;
+	
+	int l = pow(2, n);
+	int answer = 0;
 
-	int x, y;
-	while (t--)
+	while (l != 1)
 	{
-		int m, n, k;
-		cin >> m >> n >> k;
-
-		memset(farm, 0, sizeof(farm));
-		while (k--)
+		// QUARTER °è»ê
+		// O|1
+		// ---
+		// 2|3
+		int quarter = 0;
+		if (r >= l / 2)
 		{
-			cin >> x >> y;
-			farm[x][y] = 1;
+			quarter += 2;
+			r /= 2;
+		}
+		if (c >= l / 2)
+		{
+			quarter += 1;
+			c /= 2;
 		}
 
-		for (int i = 0; i < 50; i++)
-			for (int j = 0; j < 50; j++)
-				dfs(i, j);
-
-		cout << answer << '\n';
-		answer = 0;
+		// 1 4 16 ...
+		// 2^0 2^2 2^4 ...
+		answer += pow(2, n * 2);
+		cout << l << '/' << answer << '\n';
+		l /= 2;
 	}
+
+	cout << answer;
 
 	return 0;
 }
