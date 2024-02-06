7923e6ecf447a7ad569d4a5460ce8ac7e8e2aad4
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-02-07 02:03:02 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 #include <cmath>
+#include <climits>
 using namespace std;
+
+int count_digits_for_n(int n)
+{
+	if (n == 0) return 1;
+
+	int digits = 0;
+	while (n)
+	{
+		digits++;
+		n /= 10;
+	}
+
+	return digits;
+}
+
+bool cannot_goto_channel(vector<int> &v, int n)
+{
+	if (n == 0)
+	{
+		if (find(v.begin(), v.end(), 0) != v.end()) return true;
+	}
+
+	while (n)
+	{
+		int digit = n % 10;
+		n /= 10;
+
+		if (find(v.begin(), v.end(), digit) != v.end()) return true;
+	}
+
+	return false;
+}
 
 int main()
 {
@@ -8,36 +44,34 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int n, r, c;
-	cin >> n >> r >> c;
-	
-	int l = pow(2, n);
-	int answer = 0;
+	int n;
+	cin >> n;
+	int m;
+	cin >> m;
 
-	while (l != 1)
+	int input;
+	vector<int> v;
+	for (int i = 0; i < m; i++)
 	{
-		// QUARTER 계산
-		// O|1
-		// ---
-		// 2|3
-		int quarter = 0;
-		if (r >= l / 2)
-		{
-			quarter += 2;
-			r /= 2;
-		}
-		if (c >= l / 2)
-		{
-			quarter += 1;
-			c /= 2;
-		}
-
-		// 1 4 16 ...
-		// 2^0 2^2 2^4 ...
-		answer += pow(2, n * 2);
-		cout << l << '/' << answer << '\n';
-		l /= 2;
+		cin >> input;
+		v.push_back(input);
 	}
+
+	int n_digits = count_digits_for_n(n);
+	int answer = INT_MAX;
+	int diff;
+	for (int i = 0; i <= 999999; i++)
+	{
+		if (cannot_goto_channel(v, i)) continue;
+
+		diff = abs(i - n) + n_digits;
+		if (diff < answer) answer = diff;
+	}
+
+	// 모든 숫자 버튼이 고장났을 경우
+	// 100에서 시작하는 것이 더 빠른 경우.
+	diff = abs(100 - n);
+	if (diff < answer) answer = diff;
 
 	cout << answer;
 
