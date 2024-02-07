16343ec3f7456808a143f5612b6ec88783061ea2
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-02-08 00:11:45 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,42 +1,8 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
 #include <climits>
+#include <algorithm>
+#include <vector>
 using namespace std;
-
-int count_digits_for_n(int n)
-{
-	if (n == 0) return 1;
-
-	int digits = 0;
-	while (n)
-	{
-		digits++;
-		n /= 10;
-	}
-
-	return digits;
-}
-
-bool cannot_goto_channel(vector<int> &v, int n)
-{
-	if (n == 0)
-	{
-		if (find(v.begin(), v.end(), 0) != v.end()) return true;
-	}
-
-	while (n)
-	{
-		int digit = n % 10;
-		n /= 10;
-
-		if (find(v.begin(), v.end(), digit) != v.end()) return true;
-	}
-
-	return false;
-}
 
 int main()
 {
@@ -44,36 +10,24 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
+	vector<int> cache(1000001);
+	cache[1] = 0;
+	cache[2] = 1;
+	cache[3] = 1;
+	for (int i = 4; i <= 1000000; i++)
+	{
+		int temp = INT_MAX;
+		if (i % 3 == 0) temp = min(temp, cache[i / 3] + 1);
+		if (i % 2 == 0) temp = min(temp, cache[i / 2] + 1);
+		temp = min(temp, cache[i - 1] + 1);
+
+		cache[i] = temp;
+	}
+
 	int n;
 	cin >> n;
-	int m;
-	cin >> m;
 
-	int input;
-	vector<int> v;
-	for (int i = 0; i < m; i++)
-	{
-		cin >> input;
-		v.push_back(input);
-	}
-
-	int n_digits = count_digits_for_n(n);
-	int answer = INT_MAX;
-	int diff;
-	for (int i = 0; i <= 999999; i++)
-	{
-		if (cannot_goto_channel(v, i)) continue;
-
-		diff = abs(i - n) + n_digits;
-		if (diff < answer) answer = diff;
-	}
-
-	// 모든 숫자 버튼이 고장났을 경우
-	// 100에서 시작하는 것이 더 빠른 경우.
-	diff = abs(100 - n);
-	if (diff < answer) answer = diff;
-
-	cout << answer;
+	cout << cache[n];
 
 	return 0;
 }
