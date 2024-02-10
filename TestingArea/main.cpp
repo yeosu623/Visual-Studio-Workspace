b94e38e73fb1ee2057153e75a3ed78ca836f4691
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-02-11 06:48:18 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 #include <algorithm>
 using namespace std;
 
@@ -8,24 +10,38 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int n;
-	cin >> n;
+	int a;
+	cin >> a;
 
-	int input;
-	int cache[500][500];
+	int b;
+	int sum = 0;
+	vector<int> v;
+	vector<int> count(8001);
+	for (int i = 0; i < a; i++)
+	{
+		cin >> b;
+		sum += b;
+		count[b + 4000]++;
+		v.push_back(b);
+	}
+	sort(v.begin(), v.end());
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j <= i; j++)
+	int avg = round(sum / (double)v.size());
+	cout << avg << '\n';
+	cout << v[(0 + v.size() - 1) / 2] << '\n';
+
+	int idx = max_element(count.begin(), count.end()) - count.begin();
+	int max = count[idx];
+
+	for (int i = idx + 1; i < 8001; i++)
+		if (count[i] == max)
 		{
-			cin >> input;
-			cache[i][j] = input;
+			idx = i;
+			break;
 		}
-
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n - 1 - i; j++)
-			cache[n - 2 - i][j] += max(cache[n - 1 - i][j], cache[n - 1 - i][j + 1]);
-
-	cout << cache[0][0];
+	idx -= 4000;
+	cout << idx << '\n';
+	cout << v[v.size() - 1] - v[0] << '\n';
 
 	return 0;
 }
