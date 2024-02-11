226486d1fa89ad9d3d57aa1ca2eb4437727623f2
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-02-12 00:49:09 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,47 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
+#include <climits>
 using namespace std;
 
 int main()
 {
 	cin.tie(NULL);
-	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
 	int a;
 	cin >> a;
 
-	int b;
-	int sum = 0;
-	vector<int> v;
-	vector<int> count(8001);
+	int input;
+	int maxSum = INT_MIN;
+	int currentSum = 0;
 	for (int i = 0; i < a; i++)
 	{
-		cin >> b;
-		sum += b;
-		count[b + 4000]++;
-		v.push_back(b);
+		cin >> input;
+
+		currentSum += input;
+		if (currentSum > maxSum) maxSum = currentSum;
+		if (currentSum < 0) currentSum = 0;
 	}
-	sort(v.begin(), v.end());
 
-	int avg = round(sum / (double)v.size());
-	cout << avg << '\n';
-	cout << v[(0 + v.size() - 1) / 2] << '\n';
-
-	int idx = max_element(count.begin(), count.end()) - count.begin();
-	int max = count[idx];
-
-	for (int i = idx + 1; i < 8001; i++)
-		if (count[i] == max)
-		{
-			idx = i;
-			break;
-		}
-	idx -= 4000;
-	cout << idx << '\n';
-	cout << v[v.size() - 1] - v[0] << '\n';
-
+	cout << maxSum;
 	return 0;
 }
