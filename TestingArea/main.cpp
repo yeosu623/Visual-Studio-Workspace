b1ba8e84f3471358ea64d8ac4f754bb9a9d686bd
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-02-13 05:51:49 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
-#include <climits>
+#include <string>
+#include <cstring>
 using namespace std;
+
+int cache[1001][1001];
 
 int main()
 {
 	cin.tie(NULL);
+	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int a;
-	cin >> a;
 
-	int input;
-	int maxSum = INT_MIN;
-	int currentSum = 0;
-	for (int i = 0; i < a; i++)
-	{
-		cin >> input;
-
-		currentSum += input;
-		if (currentSum > maxSum) maxSum = currentSum;
-		if (currentSum < 0) currentSum = 0;
-	}
-
-	cout << maxSum;
-	return 0;
 }
