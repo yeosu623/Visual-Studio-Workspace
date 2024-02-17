b60e6feb079afa850948eaf2bc8e26acb96ca5e9
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-02-18 06:28:46 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,30 +1,65 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
 int main()
 {
 	cin.tie(NULL);
+	cout.tie(NULL);
 	ios::sync_with_stdio(false);
+
+	vector<int> v;
+	vector<char> w;
 
 	int a;
 	cin >> a;
 
 	int b;
-	int min = 99999;
-	for (int i = 0; i <= 1667; i++)
+	for (int i = 0; i < a; i++)
 	{
-		for (int j = 0; j <= 1000; j++)
-		{
-			b = 3 * i + 5 * j;
-			if (b == a)
-			{
-				if (i + j < min) min = i + j;
-			}
-		}
+		cin >> b;
+		v.push_back(b);
 	}
 
-	if (min == 99999) cout << -1;
-	else cout << min;
+	int c;
+	cin >> c;
+	for (int i = 0; i < c; i++)
+		w.push_back('+');
+	cin >> c;
+	for (int i = 0; i < c; i++)
+		w.push_back('-');
+	cin >> c;
+	for (int i = 0; i < c; i++)
+		w.push_back('*');
+	cin >> c;
+	for (int i = 0; i < c; i++)
+		w.push_back('/');
+	sort(w.begin(), w.end());
+
+	int max = INT_MIN;
+	int min = INT_MAX;
+	vector<int> operand;
+	char op;
+	do
+	{
+		operand.clear();
+		operand = v;
+		for (int i = 0; i < v.size() - 1; i++)
+		{
+			op = w[i];
+			if (op == '+') operand[i + 1] = operand[i] + operand[i + 1];
+			else if (op == '-') operand[i + 1] = operand[i] - operand[i + 1];
+			else if (op == '*') operand[i + 1] = operand[i] * operand[i + 1];
+			else if (op == '/') operand[i + 1] = operand[i] / operand[i + 1];
+		}
+
+		if (operand[v.size() - 1] > max) max = operand[v.size() - 1];
+		if (operand[v.size() - 1] < min) min = operand[v.size() - 1];
+	} while (next_permutation(w.begin(), w.end()));
+
+	cout << max << '\n' << min;
 
 	return 0;
 }
