437e0cdf6b5587ff3f91a2bbaa64248f92f832a3
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-03-01 00:03:50 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -6,4 +9,19 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
+
+	int cache[50001];
+	cache[0] = 0;
+	for (int i = 1; i <= 50000; i++)
+	{
+		int answer = INT_MAX;
+		for (int j = 1; j <= sqrt(i); j++)
+			answer = min(answer, cache[i - j*j] + 1);
+		cache[i] = answer;
+	}
+
+	int n;
+	cin >> n;
+	cout << cache[n];
+	return 0;
 }
