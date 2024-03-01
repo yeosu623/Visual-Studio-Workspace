976fe7bca6f68098f2129f3303a28e1017deaf35
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-03-02 01:43:26 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <algorithm>
-#include <climits>
-#include <cmath>
+#include <queue>
+#include <vector>
 using namespace std;
 
 int main()
@@ -10,18 +9,49 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int cache[50001];
-	cache[0] = 0;
-	for (int i = 1; i <= 50000; i++)
+	int t;
+	cin >> t;
+	while (t--)
 	{
-		int answer = INT_MAX;
-		for (int j = 1; j <= sqrt(i); j++)
-			answer = min(answer, cache[i - j*j] + 1);
-		cache[i] = answer;
+		int k;
+		cin >> k;
+
+		char input1;
+		int input2;
+		int len = 0, del = 0;
+		priority_queue<int> pq1; // 최대값 제거
+		priority_queue<int, vector<int>, greater<int>> pq2; // 최소값 제거
+		for (int i = 0; i < k; i++)
+		{
+			cin >> input1;
+			cin >> input2;
+
+			if (input1 == 'I')
+			{
+				pq1.push(input2);
+				pq2.push(input2);
+				len++;
+			}
+			else if (input1 == 'D')
+			{
+				if (pq1.empty() || pq2.empty()) continue;
+				else if (input2 == 1) pq1.pop();
+				else if (input2 == -1) pq2.pop();
+				del++;
+					
+				if (del == len)
+				{
+					while (!pq1.empty()) pq1.pop();
+					while (!pq2.empty()) pq2.pop();
+					len = 0;
+					del = 0;
+				}
+			}
+		}
+
+		if (pq1.empty() || pq2.empty()) cout << "EMPTY" << '\n';
+		else cout << pq1.top() << ' ' << pq2.top() << '\n';
 	}
 
-	int n;
-	cin >> n;
-	cout << cache[n];
 	return 0;
 }
