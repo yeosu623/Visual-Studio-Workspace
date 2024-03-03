3a75ba4524c2dcab29462caf4fff195aac975a4e
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-03-04 00:24:13 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <cstring>
 #include <queue>
 using namespace std;
+
+int map[1000][1000];
+int answer[1000][1000];
 
 int main()
 {
@@ -8,49 +12,59 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int n;
-	cin >> n;
+	int n, m;
+	cin >> n >> m;
 
-	char input;
-	char art[100][100];
-	for(int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
+	int input;
+	int sy, sx;
+	memset(answer, -1, sizeof(answer));
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
 		{
 			cin >> input;
-			art[i][j] = input;
+			map[i][j] = input;
+			if (input == 0) answer[i][j] = 0;
+			if (input == 2)
+			{
+				sy = i;
+				sx = j;
+			}
 		}
 
-	int dy[4] = {1, 0, 0, -1};
+	int dy[4] = { 1, 0, 0, -1 };
 	int dx[4] = { 0, 1, -1, 0 };
-	int art_human_region = 0, art_cow_region = 0;
-	char color;
-	queue<pair<short, short>> que;
-	for(int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
+	queue<pair<int, int>> que;
+	que.push({ sy, sx });
+	answer[sy][sx] = 0;
+
+	while (!que.empty())
+	{
+		int y = que.front().first;
+		int x = que.front().second;
+		que.pop();
+
+		for (int i = 0; i < 4; i++)
 		{
-			color = art[i][j];
-			if (color != 'O')
-			{
-				que.push({ i, j });
-				art_human_region++;
-			}
-			while (!que.empty())
-			{
-				int y = que.front().first;
-				int x = que.front().second;
-				art[y][x] = 'O';
-				que.pop();
+			int my = y + dy[i];
+			if (my < 0 || my >= n) continue;
+			int mx = x + dx[i];
+			if (mx < 0 || mx >= m) continue;
 
-				for (int i = 0; i < 4; i++)
-				{
-					int my = y + dy[i];
-					if (y < 0 || y >= n) continue;
-					int mx = x + dx[i];
-					if (x < 0 || x >= n) continue;
-
-					if (art[my][mx] == color)
-						que.push({ my, mx });
-				}
+			if (map[my][mx] == 1 && answer[my][mx] == -1)
+			{
+				answer[my][mx] = answer[y][x] + 1;
+				que.push({ my, mx });
 			}
 		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << answer[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+	return 0;
 }
