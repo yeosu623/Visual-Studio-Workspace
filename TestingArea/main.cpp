4d4900d51fa82ca1745e90849645aece7dff6600
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-03-18 00:06:18 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include <cstring>
-#include <climits>
+#include <vector>
 #include <algorithm>
 using namespace std;
-
-int cache[500][500][3]; // r, c, dp
 
 int main()
 {
@@ -12,42 +9,23 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int n;
-	cin >> n;
+	int a;
+	cin >> a;
 
-	// layer0
-	int input1, input2;
-	memset(cache, 0, sizeof(cache));
-	for (int i = 0; i < n; i++)
+	int b;
+	vector<int> v;
+	for (int i = 0; i < a; i++)
 	{
-		cin >> input1 >> input2;
-		cache[0][i][0] = input1;
-		cache[0][i][1] = input2;
+		cin >> b;
+		v.push_back(b);
 	}
 
-	// layer1 ~
-	int y1, x1, y2, x2;
-	for (int y = 1; y < n; y++)
-		for (int x = 0; x < n - y; x++)
-		{
-			cache[y][x][0] = cache[y - 1][x][0];
-			cache[y][x][1] = cache[y - 1][x + 1][1];
+	vector<int> w = v;
+	sort(w.begin(), w.end());
+	w.erase(unique(w.begin(), w.end()), w.end());
 
-			int dp_min = INT_MAX;
-			for (int i = 0; i < y; i++)
-			{
-				y1 = y - 1 - i;
-				x1 = x;
-				y2 = i;
-				x2 = y + x - i;
+	for (const auto& n : v)
+		cout << lower_bound(w.begin(), w.end(), n) - w.begin() << ' ';
 
-				int matrix = cache[y1][x1][0] * cache[y2][x2][0] * cache[y2][x2][1];
-				int dp = cache[y1][x1][2] + cache[y2][x2][2] + matrix;
-				dp_min = min(dp_min, dp);
-			}
-			cache[y][x][2] = dp_min;
-		}
-
-	cout << cache[n - 1][0][2];
 	return 0;
 }
