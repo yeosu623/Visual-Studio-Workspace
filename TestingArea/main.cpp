c20aa628b9121ac82df796764ca380c261116a76
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-03-24 10:29:41 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <algorithm>
+#include <stack>
 using namespace std;
 
 int main()
@@ -9,26 +10,52 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int n;
-	cin >> n;
+	string s, bomb;
+	cin >> s >> bomb;
+	reverse(bomb.begin(), bomb.end());
 
-	int input;
-	int v[300];
-	for (int i = 0; i < n; i++)
+	char bomb_begin = bomb[0];
+	stack<char> st;
+	stack<char> buffer;
+	for (int i = 0; i < s.length(); i++)
 	{
-		cin >> input;
-		v[i] = input;
+		st.push(s[i]);
+		if (st.top() == bomb_begin && st.size() >= bomb.length())
+		{
+			bool isBomb = true;
+			for (int i = 0; i < bomb.length(); i++)
+			{
+				if (st.top() == bomb[i])
+				{
+					buffer.push(st.top());
+					st.pop();
+				}
+				else
+				{
+					isBomb = false;
+					break;
+				}
+			}
+
+			while (!buffer.empty())
+			{
+				if (!isBomb) st.push(buffer.top());
+				buffer.pop();
+			}
+		}
 	}
 
-	int cache[300];
-	cache[0] = v[0];
-	cache[1] = v[0] + v[1];
-	cache[2] = max(v[0] + v[2], v[1] + v[2]);
-	for (int i = 3; i < n; i++)
-		cache[i] = max(cache[i - 3] + v[i - 1] + v[i], cache[i - 2] + v[i]);
-
-	if (n != 0) cout << cache[n - 1];
-	else cout << 0;
-
+	if (st.empty()) cout << "FRULA";
+	else
+	{
+		string answer;
+		while (!st.empty())
+		{
+			answer += st.top();
+			st.pop();
+		}
+		reverse(answer.begin(), answer.end());
+		cout << answer;
+	}
 	return 0;
 }
