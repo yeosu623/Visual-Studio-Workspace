835888ff4c3915ce7273297385f1c26c65fb35b5
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-03-26 00:38:06 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
-#include <stack>
 using namespace std;
+
+int gcd(int a, int b)
+{
+	int c;
+	while (b)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
 
 int main()
 {
@@ -10,52 +19,13 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	string s, bomb;
-	cin >> s >> bomb;
-	reverse(bomb.begin(), bomb.end());
+	int a, b;
+	cin >> a >> b;
 
-	char bomb_begin = bomb[0];
-	stack<char> st;
-	stack<char> buffer;
-	for (int i = 0; i < s.length(); i++)
-	{
-		st.push(s[i]);
-		if (st.top() == bomb_begin && st.size() >= bomb.length())
-		{
-			bool isBomb = true;
-			for (int i = 0; i < bomb.length(); i++)
-			{
-				if (st.top() == bomb[i])
-				{
-					buffer.push(st.top());
-					st.pop();
-				}
-				else
-				{
-					isBomb = false;
-					break;
-				}
-			}
+	int G = gcd(a, b);
+	int L = a * b / G;
 
-			while (!buffer.empty())
-			{
-				if (!isBomb) st.push(buffer.top());
-				buffer.pop();
-			}
-		}
-	}
+	cout << G << ' ' << L;
 
-	if (st.empty()) cout << "FRULA";
-	else
-	{
-		string answer;
-		while (!st.empty())
-		{
-			answer += st.top();
-			st.pop();
-		}
-		reverse(answer.begin(), answer.end());
-		cout << answer;
-	}
 	return 0;
 }
