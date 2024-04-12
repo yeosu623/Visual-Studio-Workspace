10d42c223e85df60ffc9372a49413b352883a1e4
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-04-13 00:25:51 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,45 +1,27 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-#define ll long long
-
-typedef vector<vector<ll>> matrix;
-
-const ll mod = 1000000007;
-ll N;
-matrix A = { {1, 1}, {1, 0} };
-matrix ans = { {1, 0}, {0, 1} };
-
-matrix operator * (matrix& a, matrix& b) {
-	matrix c = { {0, 0}, {0, 0} };
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 2; j++) {
-			for (int k = 0; k < 2; k++)
-				c[i][j] += a[i][k] * b[k][j];
-			c[i][j] = c[i][j] % mod;
-		}
-	}
-	return c;
-}
-
 int main() {
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N;
+	int x;
+	cin >> x;
 
-	while (N > 0) {
-		if ((N & 1) == 1) {
-			ans = ans * A;
+	int stick[] = {64, 32, 16, 8, 4, 2, 1};
+	int length = 0;
+	int answer = 0;
+	for (const auto& n : stick)
+	{
+		if (length <= x)
+		{
+			length += x;
+			answer++;
 		}
-		A = A * A;
-		N = N >> 1;
+		if (length == x) break;
 	}
 
-	cout << ans[0][1] << '\n';
-
+	cout << answer;
 	return 0;
 }
