f06b7f154b42f8a16e4e9c8d36adfbc6c0240051
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-05-27 00:13:57 +0900

    code update.

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,32 +1,8 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 using namespace std;
-
-int image[64][64];
-
-void recursion(int x1, int y1, int x2, int y2, int len, int quarter)
-{
-	int allBlack = len * len;
-	int black = 0;
-	for (int i = y1; i < y2; i++)
-		for (int j = x1; j < x2; j++)
-			black += image[i][j];
-
-	if (quarter == 1) cout << '('; // recursion ½ÃÀÛ
-
-	if (black == 0) cout << '0';
-	else if (black == allBlack) cout << '1';
-	else
-	{
-		len = len / 2;
-		recursion(x1, y1, x2 - len, y2 - len, len, 1);
-		recursion(x2 - len, y1, x2, y2 - len, len, 2);
-		recursion(x1, y2 - len, x2 - len, y2, len, 3);
-		recursion(x2 - len, y2 - len, x2, y2, len, 4);
-	}
-
-	if (quarter == 4) cout << ')'; // recursion ³¡
-}
 
 int main()
 {
@@ -34,43 +10,38 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int n;
-	cin >> n;
+	int a;
+	cin >> a;
 
-	for (int i = 0; i < n; i++)
+	int b;
+	int sum = 0;
+	vector<int> v;
+	vector<int> count(8001);
+	for (int i = 0; i < a; i++)
 	{
-		string s;
-		cin >> s;
-		for (int j = 0; j < n; j++)
-			image[i][j] = s[j] - '0';
+		cin >> b;
+		sum += b;
+		count[b + 4000]++;
+		v.push_back(b);
 	}
+	sort(v.begin(), v.end());
 
-	// all 0 or all 1
-	int allBlack = n * n;
-	int black = 0;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			black += image[i][j];
-	if (black == 0)
-	{
-		cout << '0';
-		return 0;
-	}
-	else if (black == allBlack)
-	{
-		cout << '1';
-		return 0;
-	}
+	int avg = round(sum / (double)v.size());
+	cout << avg << '\n';
+	cout << v[(0 + v.size() - 1) / 2] << '\n';
 
-	// quarter
-	// 0 | 1
-	// -----
-	// 2 | 3
-	int len = n / 2;
-	recursion(0, 0, n - len, n - len, len, 1);
-	recursion(len, 0, n, n - len, len, 2);
-	recursion(0, len, n - len, n, len, 3);
-	recursion(len, len, n, n, len, 4);
+	int idx = max_element(count.begin(), count.end()) - count.begin();
+	int max = count[idx];
+
+	for (int i = idx + 1; i < 8001; i++)
+		if (count[i] == max)
+		{
+			idx = i;
+			break;
+		}
+	idx -= 4000;
+	cout << idx << '\n';
+	cout << v[v.size() - 1] - v[0] << '\n';
 
 	return 0;
 }
