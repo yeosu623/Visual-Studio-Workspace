e70e0ee2596458d6805bf4899a037ea65a644078
yeosu623 <yeosu623@naver.com>
2025-03-27 18:43:19 +0900

    algo solved

--- TestingArea/main.cpp
+++ TestingArea/main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <numeric>
+#include <vector>
 #include <string>
 #include <cmath>
-#include <deque>
-#include <list>
-#include <stack>
+#include <numeric>
 using namespace std;
 
 int main() {
@@ -14,60 +11,9 @@ int main() {
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	string s;
-	cin >> s;
-
-	// (, ), [, ] 의 아스키 코드 숫자 구분을 위해, 숫자는 음수로 표기
-	int value;
-	bool correct = true;
-	stack<int> st;
-	for (int i = 0; i < s.size(); i++) {
-		value = -1;
-		st.push(s[i]);
-
-		switch (st.top()) {
-		case ')':
-			st.pop();
-			if (!st.empty() && st.top() < 0) {	value = st.top(); st.pop();	}
-			if (!st.empty() && st.top() == '(') { st.pop(); st.push(value * 2); }
-			else correct = false;
-			break;
-		case ']':
-			st.pop();
-			if (!st.empty() && st.top() < 0) { value = st.top(); st.pop(); }
-			if (!st.empty() && st.top() == '[') { st.pop(); st.push(value * 3); }
-			else correct = false;
-			break;
-		}
-
-		if (!st.empty() && st.top() < 0) {
-			value = st.top(); st.pop();
-			if (!st.empty() && st.top() < 0) { value += st.top(); st.pop(); st.push(value); }
-			else st.push(value);
-		}
-
-		if (!correct) break;
-		//cout << "stack : ";
-
-		//stack<int> st2(st);
-		//stack<int> st3;
-		//while (!st2.empty()) {
-		//	st3.push(st2.top());
-		//	st2.pop();
-		//}
-		//while (!st3.empty()) {
-		//	cout << st3.top() << ' ';
-		//	st3.pop();
-		//}
-		//cout << '\n';
-	}
-
-	int answer = 0;
-	while (!st.empty()) {
-		answer -= st.top();
-		st.pop();
-	}
-
-	if (correct) cout << answer;
-	else cout << 0;
-} 
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	cout << 6 * a + 3 * b + 2 * c + 1 * d + 2 * e << ' ';
+	cin >> a >> b >> c >> d >> e;
+	cout << 6 * a + 3 * b + 2 * c + 1 * d + 2 * e << ' ';
+}
